3df2bea3aa0d6e32c13168a4e017611977ae6835
Cisca <233951544+CiscaSolana@users.noreply.github.com>
2025-12-07 17:21:39 +0100

    Fix computer choice function

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 //Logic to get computer choice 
-//keep a random number between 1-100
-let randomNum = Math.floor(Math.random()*100) + 1;
-let computerChoice;
-//if random number is less than 33, the computer choice is "rock"
-if(randomNum<33){
-    computerChoice = "rock";
+let getComputerChoice = () => {
+    //keep a random number between 1-100
+    let randomNum = Math.floor(Math.random()*100) + 1;
+    let computerChoice;
+    //if random number is less than 33, the computer choice is "rock"
+    if(randomNum<33){
+        computerChoice = "rock";
+    }
+    //if random number is between 33 and 66 the computer choice is "paper" 
+    else if(randomNum>=33 && randomNum<66){
+        computerChoice = "paper";
+    }
+    //if random number is equal or more than 66, the computer choice is "scissors"
+    else{
+        computerChoice = "scissors";
+    }
 }
-//if random number is between 33 and 66 the computer choice is "paper" 
-else if(randomNum>=33 && randomNum<66){
-    computerChoice = "paper";
-}
-//if random number is equal or more than 66, the computer choice is "scissors"
-else{
-    computerChoice = "scissors";
-}
-
-console.log(computerChoice);
 
